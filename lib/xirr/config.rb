23cadf85742e7082161114170ca57b6b3a3fa2b6
Antony Ignatious <antonyignatiousforever@gmail.com>
2022-06-18 22:15:52 +0530

    Update config.rb

--- lib/xirr/config.rb
+++ lib/xirr/config.rb
@@ -1,15 +1,15 @@
 module Xirr
   include ActiveSupport::Configurable
  
-  config_accessor :eps,                 default: '1.0e-6'.to_f
-  config_accessor :period,              default: 365.25
-  config_accessor :iteration_limit,     default: 50
-  config_accessor :precision,           default: 6
-  config_accessor :default_method,      default: :newton_method
-  config_accessor :fallback,            default: true
-  config_accessor :replace_for_nil,     default: 0.0
-  config_accessor :compact,             default: true
-  config_accessor :raise_exception,     default: false
+  config_accessor :eps { '1.0e-6'.to_f }
+  config_accessor :period { 365.25 }
+  config_accessor :iteration_limit { 50 }
+  config_accessor :precision      { 6 }
+  config_accessor :default_method { :newton_method }
+  config_accessor :fallback       { true }
+  config_accessor :replace_for_nil { 0.0 }
+  config_accessor :compact        { true }
+  config_accessor :raise_exception { false }
   
 
   # Sets as constants all the entries in the Hash Default values
